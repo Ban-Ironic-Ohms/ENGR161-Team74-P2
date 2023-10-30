37a8214656edd23833ba1e78c8107d093ab05b8d
mccorm84 <145810500+mccorm84@users.noreply.github.com>
2023-10-30 21:14:53 +0000

    lol

--- operationCalculations.py
+++ operationCalculations.py
@@ -19,4 +19,21 @@ def fermenter(eff, sol):
     return sol
 
 def filter(eff, sol):
-    
+    sol.sugar = sol.sugar
+    sol.fiber = sol.fiber * (1-eff)
+    sol.water = sol.water
+    sol.ethanol = sol.ethanol
+    return sol
+
+def distiller(eff, sol):
+    sol.sugar = (sol.sugar * sol.ethanol * ((1/eff)-1))/(sol.water + sol.ethanol + sol.fiber)
+    sol.fiber = (sol.fiber * sol.ethanol * ((1/eff)-1))/(sol.water + sol.ethanol + sol.fiber)
+    sol.water = (sol.water * sol.ethanol * ((1/eff)-1))/(sol.water + sol.ethanol + sol.fiber)
+    sol.ethanol = sol.ethanol
+    return sol
+
+def dehydrator(eff, sol):
+    sol.sugar = sol.sugar
+    sol.fiber = sol.fiber
+    sol.water = sol.water * (1-eff)
+    sol.ethanol = sol.ethanol
