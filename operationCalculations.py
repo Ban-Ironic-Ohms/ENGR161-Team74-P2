de2db69557afaba3a044b0504948865bc7f39c08
Ban-Ironic-Ohms <micahrobinson93@gmail.com>
2023-10-30 16:53:43 -0400

    added ppsudocode

---
+++ operationCalculations.py
@@ -0,0 +1,14 @@
+class solution:
+    def __init__(self) -> None:
+        self.water = water
+        self.etc = etc
+
+class waste(solution):
+    def __init__(self) -> None:
+        super().__init__()
+        self.state = solid lq, gass
+    
+    
+def fermenter(eff, water_in, sugar_in, eth_in):
+    # calcualtion
+    return solution(sadasdasd, asdasd, asda asd)
