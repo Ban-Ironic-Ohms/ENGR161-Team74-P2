8666363ec4e14b6707acac2322da3418a6301ddc
mccorm84 <145810500+mccorm84@users.noreply.github.com>
2023-10-30 21:05:20 +0000

    lol

--- operationCalculations.py
+++ operationCalculations.py
@@ -1,14 +1,22 @@
 class solution:
-    def __init__(self) -> None:
+    def __init__(self, water, fiber, sugar,ethanol) -> None:
         self.water = water
-        self.etc = etc
+        self.fiber = fiber
+        self.sugar = sugar
+        self.ethanol = ethanol
 
 class waste(solution):
     def __init__(self) -> None:
         super().__init__()
-        self.state = solid lq, gass
+        self.state = solid lq, gas
     
     
-def fermenter(eff, water_in, sugar_in, eth_in):
-    # calcualtion
-    return solution(sadasdasd, asdasd, asda asd)
+def fermenter(eff, sol):
+    sol.sugar = sol.sugar * (1 - eff)
+    sol.fiber = sol.fiber
+    sol.water = sol.water
+    sol.ethanol = sol.sugar * eff
+    return sol
+
+def filter(eff, sol):
+    
