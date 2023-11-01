4b0800837a57c5b99967717a2056c1096ae3a352
mccorm84 <145810500+mccorm84@users.noreply.github.com>
2023-11-01 04:59:45 +0000

    l

--- operationCalculations.py
+++ operationCalculations.py
@@ -1,4 +1,3 @@
-#This is the solution, contianing the percent mass of the parts of the solution that make up it.
 class Solution:
     def __init__(self, water, fiber, sugar,ethanol) -> None:
         self.water = water
@@ -11,7 +10,7 @@ class Solution:
 #      super().__init__()
 #     self.state = solid lq, gas
 
-#This takes in the efficiency of the fermenter, and outputs the new solution.
+
 def fermenter(eff, sol):
     sol.sugar = sol.sugar * (1 - eff)
     sol.fiber = sol.fiber
@@ -26,7 +25,7 @@ def filt(eff, sol):
     sol.ethanol = sol.ethanol
     return sol
 
-#This takes in the efficiency of the distiller, and outputs the new solution.
+
 def distiller(eff, sol):
     sol.sugar = (sol.sugar * sol.ethanol * ((1/eff)-1))/(sol.water + sol.ethanol + sol.fiber)
     sol.fiber = (sol.fiber * sol.ethanol * ((1/eff)-1))/(sol.water + sol.ethanol + sol.fiber)
@@ -34,7 +33,7 @@ def distiller(eff, sol):
     sol.ethanol = sol.ethanol
     return sol
 
-#This takes in the efficiency of the dehydrator, and outputs the new solution.
+
 def dehydrator(eff, sol):
     sol.sugar = sol.sugar
     sol.fiber = sol.fiber
