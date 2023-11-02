1d8319f8070f7204b1e10cfaf3f9c81b91d0823d
mccorm84 <145810500+mccorm84@users.noreply.github.com>
2023-11-02 04:23:30 +0000

    lol

--- representLayout.py
+++ representLayout.py
@@ -273,19 +273,20 @@ def pumps():
     header = fid.readline()
     headers = header.strip().split(',')
     rawData = fid.readlines()
-    data = [int(i.strip().split(",")) for i in rawData]
+    data = [i.strip().split(",") for i in rawData]
     fid.close()
     pumps = []
     for i in range(1, len(data)):
         temp = []
         for j in range(1,data[i].length()):
-            temp.append(Pump(headers[j],data[i][j],)) 
-
+            temp.append(Pump(headers[j],float(data[i][j]),float(data[i][0]),int(data[0][j]))) 
+        pumps.append(temp)
+    return pumps
 
 
 operators = [Operator("Scrap", "Fermenter", 320, 46600, 0.5, oC.fermenter), Operator("Average", "Fermenter", 380, 47200, 0.75, oC.fermenter),]
 pumps1 = [
-    [Pump("Cheap", 200, 1, 6), Pump("Value", 200, 1, 6), Pump("Casdheap", 200, 1, 6)],
+    [Pump("Cheap", 200, 6,), Pump("Value", 200, 1, 6), Pump("Casdheap", 200, 1, 6)],
     [Pump("Cheap", 200, 1, 9), Pump("Value", 200, 1, 9), Pump("asd", 200, 1, 9)],   
 ]
 bends = [Bend("asd", 90, 100, 23, 0.1), Bend("asd", 90, 100, 23, 0.1), Bend("asd", 90, 100, 23, 0.1)]
